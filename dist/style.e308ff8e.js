f63186f3d2c57cebf16d9b397300cabc6d545e11
Didina Vicliuc <didinacravcenco802@gmail.com>
2022-12-05 00:14:03 +0200

    adds services home section

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63853" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61144" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
