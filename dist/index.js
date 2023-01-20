5f6f022795062c3fb2a8e7a60a1d77b196494fc1
Didina Vicliuc <didinacravcenco802@gmail.com>
2023-01-21 01:12:23 +0200

    adds services information

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54265" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54811" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
